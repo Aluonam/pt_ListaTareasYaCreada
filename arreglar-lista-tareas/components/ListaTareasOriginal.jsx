8fea63e745df5d74b3d50052e97e52a990c3f6ce
Paula Cano <paulacanoroman@gmail.com>
2023-09-18 13:29:16 +0200

    lógica handleAddTask

--- arreglar-lista-tareas/components/ListaTareasOriginal.jsx
+++ arreglar-lista-tareas/components/ListaTareasOriginal.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
 
 function ListaTareasOriginal() {
+
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
 
+//
   const handleInputChange = (event) => {
     setNewTask(event.target.value);
+    console.log("funcion input",event.target.value)
   };
 
   const handleAddTask = () => {
-    if (newTask.trim() !== '') {
-      setTasks((prevTasks) => [...prevTasks, newTask]);
-      setNewTask('');
-    }
+    // if (newTask.trim() !== '') {
+    //   setTasks((prevTasks) => [...prevTasks, newTask]);
+    //   setNewTask('');
+    // }
+    setTasks([...tasks,newTask])
+    setNewTask('')
+
   };
 
   const handleCompleteTask = (index) => {
